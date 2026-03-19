5876b5c009786cbeb0ff527ab425d7211ab4480b
Ozrachjoe85 <ozrachjoe85@gmail.com>
2026-03-19 09:35:27 -0600

    Update SonicLabApp.kt

--- app/src/main/java/com/soniclab/app/SonicLabApp.kt
+++ app/src/main/java/com/soniclab/app/SonicLabApp.kt
@@ -3,11 +3,5 @@ package com.soniclab.app
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-/**
- * SonicLab Application Class
- * 
- * This is the entry point of our app. The @HiltAndroidApp annotation
- * initializes Hilt's dependency injection system.
- */
 @HiltAndroidApp
 class SonicLabApp : Application()
