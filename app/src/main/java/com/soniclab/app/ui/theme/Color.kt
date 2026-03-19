6ff6f078dc62969d0b097df03e1033bb02997f82
Ozrachjoe85 <ozrachjoe85@gmail.com>
2026-03-19 16:07:44 -0600

    Update Color.kt

--- app/src/main/java/com/soniclab/app/ui/theme/Color.kt
+++ app/src/main/java/com/soniclab/app/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.soniclab.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// Primary colors
+// Primary Material Design 3 colors
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -11,19 +11,64 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-// Retro palette - Neon colors
+// Retro Wood Tones
+val WalnutDark = Color(0xFF2C1810)
+val WalnutMid = Color(0xFF4A2C1B)
+val WalnutLight = Color(0xFF6B4423)
+val MapleAccent = Color(0xFF8B5A2B)
+val VeneerHighlight = Color(0xFFA67C52)
+
+// Brushed Metals
+val BrushedAluminum = Color(0xFF90A4AE)
+val BrushedAluminumLight = Color(0xFFB0BEC5)
+val BrushedGold = Color(0xFFC5A572)
+val ChromeHighlight = Color(0xFFE0E0E0)
+val ChromeShadow = Color(0xFF424242)
+
+// VU Meter Colors
+val VUGreen = Color(0xFF4CAF50)
+val VUAmber = Color(0xFFFFC107)
+val VURed = Color(0xFFF44336)
+val VUNeedleRed = Color(0xFFD32F2F)
+
+// LED Indicator Colors
+val LEDGreen = Color(0xFF00FF41)
+val LEDRed = Color(0xFFFF073A)
+val LEDBlue = Color(0xFF00D9FF)
+val LEDAmber = Color(0xFFFFB020)
+val LEDOff = Color(0xFF1A1A1A)
+
+// Nixie Tube / Vacuum Tube Glow
+val NixieTubeOrange = Color(0xFFFF6E40)
+val TubeGlowWarm = Color(0xFFFF8A50)
+val TubeGlowCool = Color(0xFFFFAB70)
+
+// CRT Display / Oscilloscope Colors
+val CRTGreen = Color(0xFF39FF14)
+val CRTBlue = Color(0xFF00D9FF)
+val CRTAmber = Color(0xFFFFB020)
+val PhosphorGlow = Color(0xFF7FFF00)
+
+// Label and Engraving Colors
+val LabelWhite = Color(0xFFF5F5F5)
+val LabelGray = Color(0xFFBDBDBD)
+val LabelBlack = Color(0xFF212121)
+val EngravingGold = Color(0xFFFFD700)
+val EngravingSilver = Color(0xFFC0C0C0)
+
+// Retro Neon Colors (from original design)
 val NeonPink = Color(0xFFFF10F0)
 val NeonBlue = Color(0xFF00F0FF)
 val NeonGreen = Color(0xFF39FF14)
 val NeonOrange = Color(0xFFFF6600)
 val NeonPurple = Color(0xFFBC13FE)
 
-// Retro palette - Additional colors
+// Additional Retro Palette
 val RetroYellow = Color(0xFFFFD700)
 val RetroCyan = Color(0xFF00CED1)
 val RetroMagenta = Color(0xFFFF00FF)
 
-// Required colors from error messages
+// Required colors from screen files
 val Gray300 = Color(0xFFD1D5DB)
 val Cyan500 = Color(0xFF06B6D4)
 val Amber500 = Color(0xFFF59E0B)
@@ -31,3 +76,8 @@ val Amber500 = Color(0xFFF59E0B)
 // Background colors
 val DarkBackground = Color(0xFF0A0A0F)
 val SurfaceDark = Color(0xFF1A1A2E)
+
+// Tape Reel Colors
+val TapeReelBrown = Color(0xFF4A2C1B)
+val TapeReelBlack = Color(0xFF212121)
+val TapeReelSilver = Color(0xFFB0BEC5)
