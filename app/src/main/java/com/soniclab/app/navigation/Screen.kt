556be4ac459c2f8ed30dcecb726f14b9abd4261d
Ozrachjoe85 <ozrachjoe85@gmail.com>
2026-03-19 09:01:24 -0600

    Create Screen.kt

---
+++ app/src/main/java/com/soniclab/app/navigation/Screen.kt
@@ -0,0 +1,9 @@
+package com.soniclab.app.navigation
+
+enum class Screen(val route: String) {
+    HOME("home"),
+    LIBRARY("library"),
+    VISUALIZER("visualizer"),
+    EQUALIZER("equalizer"),
+    SETTINGS("settings")
+}
