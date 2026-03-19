6cbdafa976c54774f60d9ab7b3c258b30b2cf060
Ozrachjoe85 <ozrachjoe85@gmail.com>
2026-03-19 09:37:20 -0600

    Update WaveformVisualizer.kt

--- app/src/main/java/com/soniclab/app/ui/visualizer/WaveformVisualizer.kt
+++ app/src/main/java/com/soniclab/app/ui/visualizer/WaveformVisualizer.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
