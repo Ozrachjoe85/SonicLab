de115a689552861237d185ba09adc9957769fa90
Ozrachjoe85 <ozrachjoe85@gmail.com>
2026-03-19 18:59:23 -0600

    Update Color.kt

--- app/src/main/java/com/soniclab/app/ui/theme/Color.kt
+++ app/src/main/java/com/soniclab/app/ui/theme/Color.kt
@@ -2,82 +2,110 @@ package com.soniclab.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// Primary Material Design 3 colors
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// ============================================
+// CYBER-ANALOG-DIGITAL-COSMIC-PUNK PALETTE
+// ============================================
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
-// Retro Wood Tones
-val WalnutDark = Color(0xFF2C1810)
-val WalnutMid = Color(0xFF4A2C1B)
-val WalnutLight = Color(0xFF6B4423)
-val MapleAccent = Color(0xFF8B5A2B)
-val VeneerHighlight = Color(0xFFA67C52)
-
-// Brushed Metals
-val BrushedAluminum = Color(0xFF90A4AE)
-val BrushedAluminumLight = Color(0xFFB0BEC5)
-val BrushedGold = Color(0xFFC5A572)
-val ChromeHighlight = Color(0xFFE0E0E0)
-val ChromeShadow = Color(0xFF424242)
-
-// VU Meter Colors
-val VUGreen = Color(0xFF4CAF50)
-val VUAmber = Color(0xFFFFC107)
-val VURed = Color(0xFFF44336)
-val VUNeedleRed = Color(0xFFD32F2F)
-
-// LED Indicator Colors
-val LEDGreen = Color(0xFF00FF41)
-val LEDRed = Color(0xFFFF073A)
-val LEDBlue = Color(0xFF00D9FF)
-val LEDAmber = Color(0xFFFFB020)
-val LEDOff = Color(0xFF1A1A1A)
-
-// Nixie Tube / Vacuum Tube Glow
-val NixieTubeOrange = Color(0xFFFF6E40)
-val TubeGlowWarm = Color(0xFFFF8A50)
-val TubeGlowCool = Color(0xFFFFAB70)
-
-// CRT Display / Oscilloscope Colors
-val CRTGreen = Color(0xFF39FF14)
-val CRTBlue = Color(0xFF00D9FF)
-val CRTAmber = Color(0xFFFFB020)
-val PhosphorGlow = Color(0xFF7FFF00)
-
-// Label and Engraving Colors
-val LabelWhite = Color(0xFFF5F5F5)
-val LabelGray = Color(0xFFBDBDBD)
-val LabelBlack = Color(0xFF212121)
-val EngravingGold = Color(0xFFFFD700)
-val EngravingSilver = Color(0xFFC0C0C0)
-
-// Retro Neon Colors (from original design)
+// NEON CYBERPUNK CORE
 val NeonPink = Color(0xFFFF10F0)
-val NeonBlue = Color(0xFF00F0FF)
+val NeonCyan = Color(0xFF00F0FF)
+val NeonPurple = Color(0xFFBC13FE)
 val NeonGreen = Color(0xFF39FF14)
 val NeonOrange = Color(0xFFFF6600)
-val NeonPurple = Color(0xFFBC13FE)
+val NeonBlue = Color(0xFF00B0FF)
+val NeonYellow = Color(0xFFFFFF00)
 
-// Additional Retro Palette
-val RetroYellow = Color(0xFFFFD700)
-val RetroCyan = Color(0xFF00CED1)
-val RetroMagenta = Color(0xFFFF00FF)
+// ELECTRIC PLASMA
+val PlasmaViolet = Color(0xFF9D00FF)
+val PlasmaMagenta = Color(0xFFFF006E)
+val PlasmaBlue = Color(0xFF0080FF)
+val PlasmaCyan = Color(0xFF00FFFF)
+val PlasmaGreen = Color(0xFF00FF88)
 
-// Required colors from screen files
-val Gray300 = Color(0xFFD1D5DB)
-val Cyan500 = Color(0xFF06B6D4)
-val Amber500 = Color(0xFFF59E0B)
+// COSMIC DEEP SPACE
+val CosmicVoid = Color(0xFF0A0015)
+val CosmicPurple = Color(0xFF1A0033)
+val CosmicBlue = Color(0xFF001A33)
+val NebulaRed = Color(0xFF330011)
+val NebulaPurple = Color(0xFF220033)
 
-// Background colors
-val DarkBackground = Color(0xFF0A0A0F)
-val SurfaceDark = Color(0xFF1A1A2E)
+// DIGITAL GRID
+val GridCyan = Color(0xFF00FFFF)
+val GridPurple = Color(0xFFAA00FF)
+val GridPink = Color(0xFFFF00AA)
+val GridGreen = Color(0xFF00FF00)
+val GridWhite = Color(0xFFFFFFFF)
 
-// Tape Reel Colors
-val TapeReelBrown = Color(0xFF4A2C1B)
-val TapeReelBlack = Color(0xFF212121)
-val TapeReelSilver = Color(0xFFB0BEC5)
+// ANALOG WARMTH OVERLAY
+val AnalogAmber = Color(0xFFFFAA00)
+val AnalogGold = Color(0xFFFFD700)
+val AnalogOrange = Color(0xFFFF8800)
+val TubeGlow = Color(0xFFFF9955)
+
+// GLITCH EFFECTS
+val GlitchRed = Color(0xFFFF0055)
+val GlitchCyan = Color(0xFF00FFAA)
+val GlitchPurple = Color(0xFFAA00FF)
+val GlitchWhite = Color(0xFFFFFFFF)
+
+// HOLOGRAPHIC SHIMMER
+val HoloBlue = Color(0xFF4DFFFF)
+val HoloPink = Color(0xFFFF4DFF)
+val HoloGreen = Color(0xFF4DFF4D)
+val HoloPurple = Color(0xFFCC4DFF)
+
+// ENERGY CORE
+val EnergyCore = Color(0xFFFFFFFF)
+val EnergyRing = Color(0xFF00FFFF)
+val EnergyPulse = Color(0xFFFF00FF)
+val EnergyFlare = Color(0xFFFFAA00)
+
+// VU METER COSMIC
+val VUCosmicLow = Color(0xFF00FFAA)
+val VUCosmicMid = Color(0xFFFFAA00)
+val VUCosmicHigh = Color(0xFFFF0066)
+val VUCosmicPeak = Color(0xFFFFFFFF)
+
+// DISPLAY TECH
+val CRTGreen = Color(0xFF39FF14)
+val LCDBlue = Color(0xFF0080FF)
+val LEDCyan = Color(0xFF00FFFF)
+val OLEDWhite = Color(0xFFFFFFFF)
+val PlasmaDisplay = Color(0xFFFF00FF)
+
+// SCANLINE OVERLAY
+val ScanlineDark = Color(0xFF000000)
+val ScanlineLight = Color(0xFF111111)
+
+// CIRCUIT BOARD
+val CircuitGreen = Color(0xFF00AA44)
+val CircuitGold = Color(0xFFFFCC00)
+val CircuitCopper = Color(0xFFDD7700)
+val SolderSilver = Color(0xFFCCCCCC)
+
+// TEXT & UI
+val TextPrimary = Color(0xFFFFFFFF)
+val TextSecondary = Color(0xFF00FFFF)
+val TextAccent = Color(0xFFFF00FF)
+val TextWarning = Color(0xFFFFAA00)
+val TextError = Color(0xFFFF0055)
+
+// BACKGROUNDS
+val BgDeepSpace = Color(0xFF000011)
+val BgNebula = Color(0xFF110022)
+val BgVoid = Color(0xFF0A0015)
+val BgPanel = Color(0xFF1A0033)
+
+// MATERIAL 3 COMPATIBILITY
+val Purple80 = NeonPurple
+val PurpleGrey80 = PlasmaViolet
+val Pink80 = NeonPink
+
+val Purple40 = PlasmaViolet
+val PurpleGrey40 = CosmicPurple
+val Pink40 = PlasmaMagenta
+
+// Required from previous (keep for compatibility)
+val Gray300 = Color(0xFF888888)
+val Cyan500 = NeonCyan
+val Amber500 = AnalogAmber
